a978b290f527c3852fe4caa26b7da9fe016405d2
juno-junho <ssmm0205@naver.com>
2022-10-15 00:25:20 +0900

    learned Assertions: arrays, iterables and lines

--- udemy/spring-boot-unit-testing-main/1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ udemy/spring-boot-unit-testing-main/1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -2,6 +2,8 @@ package com.luv2code.junitdemo;
 
 import org.junit.jupiter.api.*;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 //@DisplayNameGeneration(DisplayNameGenerator.IndicativeSentences.class)
@@ -42,7 +44,7 @@ class DemoUtilsTest {
     void testSameAndNotSame() {
         String str = "luv2code";
         assertSame(demoUtils.getAcademy(), demoUtils.getAcademyDuplicate(), "Objects should refer to same object");
-        assertNotSame(str, demoUtils.getAcademy(),"Objects should not refer to same object");
+        assertNotSame(str, demoUtils.getAcademy(), "Objects should not refer to same object");
     }
 
     @DisplayName("True and False")
@@ -53,6 +55,42 @@ class DemoUtilsTest {
 
         assertTrue(demoUtils.isGreater(gradeOne, gradeTwo), "This should return true");
         assertFalse(demoUtils.isGreater(gradeTwo, gradeOne), "This should return false");
+    }
+
+    @DisplayName("Arrays Equals")
+    @Test
+    void testArrayEquals() {
+        String[] stringArray = {"A", "B", "C"};
+
+        assertArrayEquals(stringArray, demoUtils.getFirstThreeLettersOfAlphabet(), "Arrays should be the same");
+    }
+
+    @DisplayName("Iterable equals")
+    @Test
+    void testIterableEquals() {
+        // java.lang.Iterable 인터페이스를 구현한 클래스 (ArrayList, LinkedList, HashSet, TreeSet ...)를 비교. 동일한 유형일 필요 없음.
+        List<String> theList = List.of("luv", "2", "code");
+
+        assertIterableEquals(theList, demoUtils.getAcademyInList(), "Expected list should be same as actual list");
+    }
+
+    @DisplayName("Lines match")
+    @Test
+    void testLinesMatch() {
+        // assertIterableEquals랑 다른 점은 예상 줄이 실제 줄과 같은지를 확인. 같으면 다음 쌍으로 이동.
+        List<String> theList = List.of("luv", "2", "code");
+
+        assertLinesMatch(theList, demoUtils.getAcademyInList(), "Lines should match");
+    }
+    @Test
+    void whenAssertingEqualityListOfStrings_thenEqual() {
+
+        List<String> expected = List.of("Java", "\\d+", "JUnit");
+
+        List<String> actual = List.of("Java", "11", "JUnit");
+        System.out.println(expected.toString());
+        System.out.println(actual);
+        assertLinesMatch(expected, actual);
     }
     /*
     @AfterEach
